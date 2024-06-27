edc60ad26e90b33b98d56407c91d26b6cefab138
Lucas <l.verdiell@gmail.com>
2024-06-27 17:56:01 +0100

    style: fixes on resume

--- src/components/WorkExperienceCard.tsx
+++ src/components/WorkExperienceCard.tsx
@@ -9,12 +9,12 @@ type Props = {
 function WorkExperienceCard({ title, description, date }: Props) {
   return (
     <Card className='border-none shadow-none py-2 flex flex-row gap-2'>
-      <div className='flex-[3] flex flex-col justify-evenly'>
-        <div className='flex flex-row justify-start gap-6'>
+      <div className='flex-[3] flex flex-col justify-evenly gap-2'>
+        <div className='flex flex-col sm:flex-row justify-start gap-2 md:gap-6'>
           <p className='text font-bold'>{title}</p>
           <i className='text-muted-foreground text-sm'>{date}</i>
         </div>
-        <p className='text-sm line-clamp-4 text-justify'>{description}</p>
+        <p className='text-sm text-justify'>{description}</p>
       </div>
     </Card>
   );
